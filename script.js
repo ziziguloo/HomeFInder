84814ef9ab2333eb4b479eb2afa50cf53564be79
ziziguloo <92766742+ziziguloo@users.noreply.github.com>
2022-07-20 09:29:32 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,64 @@
+var mehdiAminipouri = "3078 Cardinal Court, Coquitlam";
+var mohsenAlimomeni = "1670 Windemere Place, Port Couquitlam";
+var mohammadRezaYavari = "9101 - Unit 40 Forest Grove Drive, Burnaby";
+var saeedHosseini = "Unit 92 - 2450 Hawthrone Ave, Port Coquitlam";
+var hamidChavoshi = "2966 Coyote Court, Coquitlam";
+var aliBazyar = "3105 Azure Court, Coquitlam";
+var hadiKamali = "1292 Sherman Street, Coquitlam";
+var meysamMirahmadi = "62 Ravine Drive, Coquitlam";
+var matinRahmatian = "1252 Halifax Avenue, Port Coquitlam";
+var hamidRezazadeh = "4320 Coventry Drive, Richmond";
+
+userInput = prompt(
+  "Where would you like to go? Please Enter Last name starting with a capital. (eg. Aminipouri). If nothing shows up please enter 'cancel', then refresh page."
+);
+
+// Aminipouri Fam
+if (userInput === "Aminipouri") {
+  alert(mehdiAminipouri);
+}
+
+// Alimomeni Fam
+if (userInput === "Alimomeni") {
+  alert(mohsenAlimomeni);
+}
+
+//Hosseini Fam
+if (userInput === "Hoisseini") {
+  alert(saeedHosseini);
+}
+
+//Chavoshi Fam
+if (userInput === "Chavoshi") {
+  alert(hamidChavoshi);
+}
+
+//Bazyar Fam
+if (userInput === "Bazyar") {
+  alert(aliBazyar);
+}
+
+//Kamali Fam
+if (userInput === "Kamali") {
+  alert(hadiKamali);
+}
+
+//Mirahmadi Fam
+if (userInput === "Mirahmadi") {
+  alert(meysamMirahmadi);
+}
+
+//Rahmatian Fam
+if (userInput === "Rahmatian") {
+  alert(matinRahmatian);
+}
+
+//Rezazadeh Fam
+if (userInput === "Rezazadeh") {
+  alert(hamidRezazadeh);
+}
+
+//Yavari Fam
+if (userInput === "Yavari") {
+  alert(mohammadRezaYavari);
+}
